5001ab9405d2fcefa46da3407fe4bce0aa479ec0
Uwem Akpabot <akpabot.it@gmail.com>
2025-08-27 15:50:29 +0100

    Login completed

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -13,7 +13,7 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'name' => 'required|min:6|max:191',
+            'name' => 'required|min:5|max:191',
             'email' => 'required|email|max:191|unique:users,email',
             'password' => 'required|min:6',
         ]);
@@ -37,6 +37,39 @@ class AuthController extends Controller
                 'token' => $token,
                 'message' => 'Registered successfully'
             ]);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|max:191',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()){
+            return response()->json([
+                'validation_errors' => $validator->messages(),
+            ]);
+        } else {
+            $user = User::where('email', $request->email)->first(); 
+            
+            if (!$user || !Hash::check($request->password, $user->password)){
+                return response()->json([
+                    'status' => 401,
+                    'message' => 'Invalid credentials'
+                ]);
+
+            } else {
+                $token = $user->createToken($user->email.'_Token')->plainTextToken;
+            
+                return response()->json([
+                    'status' => 200,
+                    'username' => $user->name,
+                    'token' => $token,
+                    'message' => 'Logged-in successfully'
+                ]);
+            }
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
-Route::post('register', [AuthController::class, 'register']); // public registration
+Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
