1568ec9171764af94d5bc830371045eb5ad759d5
Uwem Akpabot <akpabot.it@gmail.com>
2025-08-27 18:26:45 +0100

    Logout completed

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -72,4 +72,14 @@ class AuthController extends Controller
             }
         }
     }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+        
+        return response()->json([
+            'status' => 200,
+            'message' => 'Logged out successfully'
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,7 @@ use App\Http\Controllers\Api\AuthController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::post('logout', [AuthController::class, 'logout']);
+});
