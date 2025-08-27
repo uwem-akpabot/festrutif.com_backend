f6e98374059a08afed141b96a0efd97dd8374840
Uwem Akpabot <akpabot.it@gmail.com>
2025-08-27 13:46:16 +0100

    Register completed

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -13,7 +13,7 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'name' => 'required|max:191',
+            'name' => 'required|min:6|max:191',
             'email' => 'required|email|max:191|unique:users,email',
             'password' => 'required|min:6',
         ]);
